d222e719134f3a16b1571ac652bbdea35285fd6f
chapkovski <chapkovski@gmail.com>
2018-10-29 13:21:45 +0300

    before reshufflin of market page

--- double_auction/pages.py
+++ double_auction/pages.py
@@ -6,23 +6,23 @@ import random
 
 class Market(Page):
     def vars_for_template(self):
-        bidsize, asksize = random.randint(4,10),random.randint(4,10)
+        bidsize, asksize = random.randint(4, 10), random.randint(4, 10)
         a = random.sample(range(100), bidsize)
         b = random.sample(range(100), bidsize)
-        bids=zip(a, b)
+        bids = zip(a, b)
         a = random.sample(range(100), asksize)
         b = random.sample(range(100), asksize)
-        asks= zip(a, b)
-        repsize = random.randint(3,6)
+        asks = zip(a, b)
+        repsize = random.randint(3, 6)
         a = random.sample(range(100), repsize)
         b = random.sample(range(100), repsize)
         c = random.sample(range(100), repsize)
-
-        repository= zip(a, b,c)
+        d = random.sample(range(100), repsize)
+        repository = zip(a, b, c, d)
         return {
             'bids': bids,
-            'asks':asks,
-            'repository':repository
+            'asks': asks,
+            'repository': repository
         }
 
 
